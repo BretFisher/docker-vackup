0e7edf83135bc330ed97c49680640369ca14f205
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-14 19:10:02 +0200

    „scripts/docker-volume-restore-all.sh“ ändern

--- scripts/docker-volume-restore-all.sh
+++ scripts/docker-volume-restore-all.sh
@@ -2,11 +2,74 @@
 # VOLUMES=$(docker volume ls  --format '{{.Name}}' > /opt/backup-volume/volume-list.txt)
 # VOLUMES=$(docker volume ls  --format '{{.Name}}')
 VOLUMES=$(cat /opt/backup-volume/volume-list.txt)
-DIR=/opt/backup-volume
-cd $DIR
+BDIR="$PWD"
+DIR="/opt/backup-volume"
+usage() {
+    echo "======================================"
+    echo " 1 = LIST ALL BACKUP"
+    echo " 2 = RESTORE BACKUP"
+    echo " h = HELP OUTPUT"
+    echo " e = exit"
+    read -p 'Enter value: ' value;
+}
+function BAD() {
+    echo "======================================"
+    read -p 'Enter value: ' value;
+}
+function LIST_BACKUP() {
+    cd $DIR
+    echo "======================================"
+    echo "======== ALL BACKUPS FOLDER==========="
+    echo "======================================"
+    ls | grep "backup-*"
+    echo "======================================"
+    cd $BDIR
+}
+function RESTORE_BACKUP() {
+    cd $DIR
+    echo "======================================"
+    echo "======== ALL BACKUPS FOLDER==========="
+    echo "======================================"
+    ls | grep "backup-*"
+    echo "======================================"
+    read -p 'Enter the Name of the Folder: ' BACKUP_VOLUME;
+    if [ "`ls | grep $BACKUP_VOLUME`" ]; then
+        cd $BACKUP_VOLUME
+        for VOLUME in $VOLUMES
+        do
+            echo "========================================="
+            echo "Run restore for Docker volume $VOLUME"
+            /usr/local/bin/vackup import $VOLUME.tgz $VOLUME
+            echo "========================================="
+        done
+        cd $BDIR
+    else
+        echo "Error the Name is incorrect"
+        usage;
+    fi
+}
 
-for VOLUME in $VOLUMES
+
+while [ true ];
 do
-  echo "Run restore for Docker volume $VOLUME"
-  /usr/local/bin/vackup import $VOLUME.tgz $VOLUME 
+	usage;
+    case "$value" in
+        1)
+            LIST_BACKUP
+            ;;
+        2)
+            RESTORE_BACKUP
+            # break
+            ;;
+        h)
+            usage
+            ;;
+        e)
+            exit 1
+            ;;
+        *)
+            echo "Unknown parameter"
+            BAD
+            ;;
+    esac
 done
