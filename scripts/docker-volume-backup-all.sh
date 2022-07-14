5164da4fe63825d1b7dbb87f24f89ba48438ac47
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-14 15:03:42 +0200

    „scripts/docker-volume-backup-all.sh“ hinzufügen

---
+++ scripts/docker-volume-backup-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# VOLUMES=$(docker volume ls  --format '{{.Name}}' > /opt/backup-volume/volume-list.txt)
+# VOLUMES=$(docker volume ls  --format '{{.Name}}')
+VOLUMES=$(cat /opt/backup-volume/volume-list.txt)
+BDIR="$PWD"
+DIR="/opt/backup-volume"
+DATE=$(date +%Y-%m-%d--%H-%S)
+ROTATE_DAYS=30
+cd $DIR
+# mkdir -p ./backup-${DATE} && cd $_
+mkdir -p $DIR/backup-${DATE} && cd "$_"
+for VOLUME in $VOLUMES
+do
+  echo "Run backup for Docker volume $VOLUME "
+  /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
+done
+find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
+cd $BDIR
