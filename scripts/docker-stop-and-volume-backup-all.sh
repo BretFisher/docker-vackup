13078172851cf2e69ec82bf9048f50993ab301c0
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-15 21:31:12 +0200

    „scripts/docker-stop-and-volume-backup-all.sh“ hinzufügen

---
+++ scripts/docker-stop-and-volume-backup-all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# VOLUMES=$(docker volume ls  --format '{{.Name}}' > /opt/backup-volume/volume-list.txt)
+# VOLUMES=$(docker volume ls  --format '{{.Name}}')
+VOLUMES=$(cat /opt/backup-volume/volume-list.txt)
+CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)
+BDIR="$PWD"
+DIR="/opt/backup-volume"
+DATE=$(date +%Y-%m-%d--%H-%M)
+ROTATE_DAYS=30
+cd $DIR
+for CONTAINER in $CONTAINERS
+do
+  echo "========================================="
+  echo "docker stop $CONTAINER"
+  docker stop $CONTAINER
+  echo "========================================="
+done
+mkdir -p $DIR/backup-${DATE} && cd "$_"
+for VOLUME in $VOLUMES
+do
+  echo "========================================="
+  echo "Run backup for Docker volume $VOLUME "
+  /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
+  echo "========================================="
+done
+for CONTAINER in $CONTAINERS
+do
+  echo "========================================="
+  echo "docker start $CONTAINER"
+  docker start $CONTAINER
+  echo "========================================="
+done
+find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
+cd $BDIR
