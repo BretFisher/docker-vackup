c7bc8504623fab5258d43c24b8712895653c9a80
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-19 04:53:45 +0200

    docker-stop-and-volume-backup-all.sh

--- scripts/docker-stop-and-volume-backup-all.sh
+++ scripts/docker-stop-and-volume-backup-all.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 # VOLUMES=$(docker volume ls  --format '{{.Name}}' > /opt/scripts/docker-volume-list.txt)
 # VOLUMES=$(docker volume ls  --format '{{.Name}}')
-VOLUMES=$(cat /opt/scripts/docker-volume-list.txt)
+VOLUMES="/opt/scripts/docker-volume-list.txt"
 CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)
 BDIR="$PWD"
 DIR="/opt/backup-volume"
 DATE=$(date +%Y-%m-%d--%H-%M)
 ROTATE_DAYS=30
+if [ -f "$VOLUMES" ]; then
+    echo  
+else
+    echo 
+    echo " VOLUMES File does not exist . "
+    echo " Create a File of your DOCKER VOLUMES "
+    echo " ==> docker volume ls  --format '{{.Name}}' > $VOLUMES <== "
+    echo " OR Change the variable VOLUMES= "
+    echo
+    exit 1
+fi
+if [ -d "$DIR" ]; then
+    echo
+else
+    echo 
+    echo " BACKUP Directory does not exist."
+    echo " Create a BACKUP Directory or change the variable DIR= "
+    echo " ==> mkdir -p $DIR <== "
+    echo " Or Change the variable DIR= " 
+    echo
+    exit 1
+fi
 cd $DIR
 for CONTAINER in $CONTAINERS
 do
@@ -16,7 +38,7 @@ do
   echo "========================================="
 done
 mkdir -p $DIR/backup-${DATE} && cd "$_"
-for VOLUME in $VOLUMES
+for VOLUME in $(cat $VOLUMES)
 do
   echo "========================================="
   echo "Run backup for Docker volume $VOLUME "
