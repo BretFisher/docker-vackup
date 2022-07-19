b74201603034b2809d802aa094b8734bf4b6a4b9
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-19 04:47:27 +0200

    docker-volume-backup-all.sh

--- scripts/docker-volume-backup-all.sh
+++ scripts/docker-volume-backup-all.sh
@@ -1,19 +1,41 @@
 #!/bin/bash
 # VOLUMES=$(docker volume ls  --format '{{.Name}}' > /opt/scripts/docker-volume-list.txt)
 # VOLUMES=$(docker volume ls  --format '{{.Name}}')
-VOLUMES=$(cat /opt/scripts/docker-volume-list.txt)
+VOLUMES="/opt/scripts/docker-volume-list.txt"
 BDIR="$PWD"
 DIR="/opt/backup-volume"
 DATE=$(date +%Y-%m-%d--%H-%M)
 ROTATE_DAYS=30
+if [ -f "$VOLUMES" ]; then
+    echo  
+else
+    echo 
+    echo " VOLUMES File does not exist . "
+    echo " Create a File of your DOCKER VOLUMES "
+    echo " ==> docker volume ls  --format '{{.Name}}' > $VOLUMES <== "
+    echo " OR Change the variable VOLUMES= "
+    echo 
+    exit 1	
+fi
+if [ -d "$DIR" ]; then
+    echo 
+else
+    echo 
+	  echo " BACKUP Directory does not exist."
+	  echo " Create a BACKUP Directory or change the variable DIR= "
+	  echo " ==> mkdir -p $DIR <== "
+    echo " Or Change the variable DIR= " 
+	  echo
+	exit 1
+fi
 cd $DIR
 mkdir -p $DIR/backup-${DATE} && cd "$_"
-for VOLUME in $VOLUMES
+for VOLUME in $(cat $VOLUMES)
 do
-  echo "========================================="
-  echo "Run backup for Docker volume $VOLUME "
-  /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
-  echo "========================================="
+    echo "========================================="
+    echo "Run backup for Docker volume $VOLUME "
+    /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
+    echo "========================================="
 done
 find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
 cd $BDIR
