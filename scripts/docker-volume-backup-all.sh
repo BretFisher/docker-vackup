84d564b0742cbe4b01adfa7001d68a7c154b8ca1
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-19 04:49:37 +0200

    docker-volume-backup-all.sh

--- scripts/docker-volume-backup-all.sh
+++ scripts/docker-volume-backup-all.sh
@@ -7,26 +7,26 @@ DIR="/opt/backup-volume"
 DATE=$(date +%Y-%m-%d--%H-%M)
 ROTATE_DAYS=30
 if [ -f "$VOLUMES" ]; then
-    echo  
-else
     echo 
+else
+    echo
     echo " VOLUMES File does not exist . "
     echo " Create a File of your DOCKER VOLUMES "
     echo " ==> docker volume ls  --format '{{.Name}}' > $VOLUMES <== "
     echo " OR Change the variable VOLUMES= "
-    echo 
+    echo
     exit 1	
 fi
 if [ -d "$DIR" ]; then
     echo 
 else
     echo 
-	  echo " BACKUP Directory does not exist."
-	  echo " Create a BACKUP Directory or change the variable DIR= "
-	  echo " ==> mkdir -p $DIR <== "
+    echo " BACKUP Directory does not exist."
+    echo " Create a BACKUP Directory or change the variable DIR= "
+    echo " ==> mkdir -p $DIR <== "
     echo " Or Change the variable DIR= " 
-	  echo
-	exit 1
+    echo
+    exit 1
 fi
 cd $DIR
 mkdir -p $DIR/backup-${DATE} && cd "$_"
