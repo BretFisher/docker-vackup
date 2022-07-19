0f9ee2db27c91d5df238fd5e52dfa7e879c561c6
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-19 11:38:07 +0200

    docker-stop-and-volume-backup-all.sh

--- scripts/docker-stop-and-volume-backup-all.sh
+++ scripts/docker-stop-and-volume-backup-all.sh
@@ -7,6 +7,14 @@ BDIR="$PWD"
 DIR="/opt/backup-volume"
 DATE=$(date +%Y-%m-%d--%H-%M)
 ROTATE_DAYS=30
+if [ -f "/usr/local/bin/vackup" ]; then
+    echo > /dev/null
+else
+    echo
+    echo " vackup not installed"
+    echo " curl -sSL https://raw.githubusercontent.com/alcapone1933/docker-vackup/master/vackup > /usr/local/bin/vackup && chmod +x /usr/local/bin/vackup"
+    exit 1
+fi 
 if [ -f "$VOLUMES" ]; then
     if [ -s $VOLUMES ]; then
         echo > /dev/null
@@ -39,7 +47,6 @@ else
     echo
     exit 1
 fi
-cd $DIR
 for CONTAINER in $CONTAINERS
 do
     echo "========================================="
@@ -47,13 +54,22 @@ do
     docker stop $CONTAINER
     echo "========================================="
 done
+cd $DIR
+volume_log_file="$DIR/volume_log_file.log"
+echo "" > $volume_log_file
 mkdir -p $DIR/backup-${DATE} && cd "$_"
 for VOLUME in $(cat $VOLUMES)
 do
-    echo "========================================="
-    echo "Run backup for Docker volume $VOLUME "
-    /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
-    echo "========================================="
+    DOCKER_VOLUME=$(docker volume ls  --format '{{.Name}}' | grep $VOLUME)
+    if [[ "$VOLUME" = "$DOCKER_VOLUME" ]]; then
+        echo "========================================="
+        echo " Run backup for Docker volume $VOLUME "
+        echo " BACKED UP The VOLUME     ==> $VOLUME <== in the LIST " >> $volume_log_file
+        /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
+        echo "========================================="		
+    else
+        echo " NOT BACKED UP the VOLUME ==> $VOLUME <== in the LIST " >> $volume_log_file
+    fi
 done
 for CONTAINER in $CONTAINERS
 do
@@ -62,5 +78,12 @@ do
    docker start $CONTAINER
    echo "========================================="
 done
+echo
+cat $volume_log_file
+echo
+[ ! -f "$volume_log_file" ] && echo > /dev/null || rm -fv $volume_log_file
+echo
 find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
+echo
 cd $BDIR
+
