cacdc5af637dcd2b97abd53b30d98b67d357f6c7
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-23 08:51:48 +0200

    docker-volume-restore-all.sh

--- scripts/docker-volume-restore-all.sh
+++ scripts/docker-volume-restore-all.sh
@@ -4,6 +4,8 @@
 VOLUMES="/opt/scripts/docker-volume-list.txt"
 BDIR="$PWD"
 DIR="/opt/backup-volume"
+DATE=$(date +%Y-%m-%d--%H-%M)
+ROTATE_DAYS=30
 if [ -f "/usr/local/bin/vackup" ]; then
     echo > /dev/null
 else
@@ -24,7 +26,7 @@ if [ -f "$VOLUMES" ]; then
         exit 1
     fi
 else
-    echo 
+    echo
     echo " VOLUMES File does not exist . "
     echo " Create a File of your DOCKER VOLUMES "
     echo " ==> docker volume ls --format '{{.Name}}' > $VOLUMES <== "
@@ -32,11 +34,10 @@ else
     echo
     exit 1
 fi
-
 if [ -d "$DIR" ]; then
     echo > /dev/null
 else
-    echo 
+    echo
     echo " BACKUP Directory does not exist."
     echo " Create a BACKUP Directory or change the variable DIR= "
     echo " ==> mkdir -p $DIR <== "
@@ -47,33 +48,78 @@ fi
 usage() {
     echo
     echo "======================================"
-    echo "[ 1 ] - LIST ALL BACKUP"
-    echo "[ 2 ] - RESTORE BACKUP"
-    # echo "[ 3 ] - DELETE THE CONTENTS OF THE VOLUMES BEFORE RESTORING "
-    echo "[ l ] - VOLUME LIST FILE"	
+    echo "[ 1 ] - BACKUP VOLUMES"
+    echo "[ 2 ] - LIST ALL VOLUMES BACKUP"
+    echo "[ 3 ] - RESTORE VOLUMES BACKUP"
+    echo "[ 4 ] - VOLUME LIST FILE"
+    # echo "[ 5 ] - DELETE THE CONTENTS OF THE VOLUMES BEFORE RESTORING "
     echo "[ h ] - HELP OUTPUT"
     echo "[ e ] - exit"
     echo "======================================"
     echo
-    read -p 'Enter value: ' value;
+    read -n 1 -t 60 -p 'Enter value: ' value;
 }
 function BAD() {
     echo
     echo "======================================"
     echo "Unknown parameter"
     sleep 2
-    return 1	
+    return 1
 }
 function countdown() {
-  secs=$1
-  shift
-  msg=$@
-  while [ $secs -gt 0 ]
-  do
-    printf "\r\033[KWaiting %.d seconds or Cancel ctrl+c $msg" $((secs--))
-    sleep 1
-  done
-  echo
+    secs=$1
+    shift
+    msg=$@
+    while [ $secs -gt 0 ]
+    do
+        printf "\r\033[KWaiting %.d seconds or Cancel ctrl+c $msg" $((secs--))
+        sleep 1
+    done
+    echo
+}
+function BACKUP_VOLUMES() {
+    countdown 5
+    CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)
+    for CONTAINER in $CONTAINERS
+    do
+        echo "========================================="
+        echo "docker stop ${CONTAINER}"
+        docker stop ${CONTAINER}
+        echo "========================================="
+    done
+    cd $DIR
+    volume_log_file="$DIR/volume_log_file.log"
+    echo "" > $volume_log_file
+    mkdir -p $DIR/backup-${DATE} && cd "$_"
+    for VOLUME in $(cat $VOLUMES)
+    do
+        DOCKER_VOLUME=$(docker volume ls  --format '{{.Name}}' | grep ${VOLUME}$)
+        if [[ "$VOLUME" = "$DOCKER_VOLUME" ]]; then
+            echo "========================================="
+            echo " Run backup for Docker volume $VOLUME "
+            echo " BACKED UP The VOLUME     ==> $VOLUME <== in the LIST " >> $volume_log_file
+            /usr/local/bin/vackup export $VOLUME $VOLUME.tgz
+            echo "========================================="
+        else
+            echo " NOT BACKED UP the VOLUME ==> $VOLUME <== in the LIST " >> $volume_log_file
+        fi
+    done
+    for CONTAINER in $CONTAINERS
+    do
+       echo "========================================="
+       echo "docker start ${CONTAINER}"
+       docker start ${CONTAINER}
+       echo "========================================="
+    done
+    echo
+    cat $volume_log_file
+    echo
+    [ ! -f "$volume_log_file" ] && echo > /dev/null || rm -fv $volume_log_file
+    echo
+    find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
+    echo
+    cd $BDIR
+    LIST_BACKUP
 }
 function VOLUME_LIST() {
     if [ -s $VOLUMES ]; then
@@ -115,8 +161,9 @@ function LIST_BACKUP() {
     sleep 1
 }
 function RESTORE_BACKUP() {
+    countdown 5
     cd $DIR
-    CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)    
+    CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)
     i=1
     declare -A FOLDER_SELECTION
     if [[ $(find ${DIR}/backup-* -maxdepth 1 -type d 2> /dev/null| wc -l) -lt 1 ]]; then
@@ -165,8 +212,8 @@ function RESTORE_BACKUP() {
     for CONTAINER in $CONTAINERS
     do
         echo "========================================="
-        echo " docker start $CONTAINER"
-        docker start $CONTAINER
+        echo " docker start ${CONTAINER}"
+        docker start ${CONTAINER}
         echo "========================================="
     done
     cat $volume_restor_log_file
@@ -178,19 +225,20 @@ function RESTORE_BACKUP() {
 }
 function DELETE_BEFOR_RESTORE() {
     CONTAINERS=$(docker container ls --format 'table {{.Names}}' | tail -n +2)
-    # VOLUME_LIST	
+    # VOLUME_LIST
     sleep 1
-    # if ! $1 ; 
+    # if ! $1 ;
     # then
         # return 1
     # fi
+    echo " VOLUMES BEFORE RESTORE DELETE"
     countdown 10
-    for CONTAINER in $CONTAINERS; do echo -e "\\ndocker stop $CONTAINER"; done
+    for CONTAINER in $CONTAINERS; do echo -e "\\ndocker stop ${CONTAINER}"; done
     for CONTAINER in $CONTAINERS
     do
         echo "========================================="
-        echo "docker stop $CONTAINER"
-        docker stop $CONTAINER
+        echo " docker stop ${CONTAINER}"
+        docker stop ${CONTAINER}
         echo "========================================="
     done
     for VOLUME in $(cat $VOLUMES)
@@ -209,15 +257,21 @@ function DELETE_BEFOR_RESTORE() {
 }
 while [ true ];
 do
-	usage;
+    usage;
     case "$value" in
         1)
-            LIST_BACKUP
+            BACKUP_VOLUMES
             ;;
         2)
+            LIST_BACKUP
+            ;;
+        3)
             RESTORE_BACKUP
             ;;
-        # 3)
+        4)
+            VOLUME_LIST
+            ;;
+        # 5)
             # DELETE_BEFOR_RESTORE
             # ;;
         h)
