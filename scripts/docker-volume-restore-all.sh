7574a4c4e05c859094d080541ee6191ef5e45ff1
alcapone1933 <alcapone1933@cosanostra-cloud.de>
2022-07-24 02:26:33 +0200

    docker-volume-restore-all.sh

--- scripts/docker-volume-restore-all.sh
+++ scripts/docker-volume-restore-all.sh
@@ -5,7 +5,7 @@ VOLUMES="/opt/scripts/docker-volume-list.txt"
 BDIR="$PWD"
 DIR="/opt/backup-volume"
 DATE=$(date +%Y-%m-%d--%H-%M)
-ROTATE_DAYS=30
+ROTATE_DAYS="30"
 if [ -f "/usr/local/bin/vackup" ]; then
     echo > /dev/null
 else
@@ -120,7 +120,7 @@ function BACKUP_VOLUMES() {
     echo
     [ ! -f "$volume_log_file" ] && echo > /dev/null || rm -fv $volume_log_file
     echo
-    find $DIR/backup-* -mtime +$ROTATE_DAYS -exec rm -rvf {} \;
+    find ${DIR}/backup-* -mtime +${ROTATE_DAYS} -exec rm -rvf {} \;
     echo
     cd $BDIR
     LIST_BACKUP
